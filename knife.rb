1d529f29082ecf19409e48a255472f910e005ba9
stevif <stevi.febrianto@gmail.com>
2018-05-04 11:25:48 +0000

    knife.rb config on workstation chef

--- knife.rb
+++ knife.rb
@@ -1,9 +1,10 @@
-current_dir = File.dirname(__FILE__)
-log_level		:info
-log_location		STDOUT
-node_name 		"admin"
-client_key		"/home/vagrant/admin.pem"
-validation_client_name 	"learndevops-validator"
-validation_key		"/home/vagrant/org.pem"
-chef_server_url		"https://chef.example.com/organizations/learndevops"
-cookbook_path		["/home/vagrant/cookbooks"]
+>current_dir = File.dirname(__FILE__)
+>log_level		:info
+>log_location		STDOUT
+>node_name 		"admin"
+>client_key		"/home/vagrant/admin.pem"
+>validation_client_name 	"learndevops-validator"
+>validation_key		"/home/vagrant/org.pem"
+>chef_server_url		"https://chef.example.com/organizations/learndevops"
+>cookbook_path		["/home/vagrant/cookbooks"]
+>EOF
